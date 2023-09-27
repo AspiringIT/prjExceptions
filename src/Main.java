739caf2e98c9d9955354c0a8d3b2270bbea34cd8
HappyRunner389 <nicktab20@gmail.com>
2023-09-27 13:28:47 -0500

    Continued work on array

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import java.util.ArrayList;
+
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        double[] ScoresArray  = new double[]{ 75,89,98,62,129,-70,80,90,100 };;
+        double[] ScoresArray  = new double[]{ 75,89,98,62,129,-70,80,90,100 };
     }
     }
-}
--- src/TestScores.java
+++ src/TestScores.java
@@ -1,2 +1,11 @@
 public class TestScores {
+
+    public void ScoreChecker(double[] ScoresArray) {
+        for (int i = 0; i <= 9; i++) {
+            if(ScoresArray[i] != null) {
+                max = array[i];
+            }
+
+        }
+    }
 }
