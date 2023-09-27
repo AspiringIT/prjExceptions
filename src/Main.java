d9f6ce198e22e474bf77a6edbb992eea42d3e641
HappyRunner389 <nicktab20@gmail.com>
2023-09-27 14:35:24 -0500

    Deleated main and moved it all into one class, code seems to be working just making some minor tweaks moving forward

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-
-
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        double[] ScoresArray  = new double[]{ 75,89,98,62,129,-70,80,90,100 };
-    }
-    }
--- src/TestScores.java
+++ src/TestScores.java
@@ -1,11 +1,46 @@
 public class TestScores {
+    private int[] TestScores;
 
-    public void ScoreChecker(double[] ScoresArray) {
-        for (int i = 0; i <= 9; i++) {
-            if(ScoresArray[i] != null) {
-                max = array[i];
+    //class constructor
+    public TestScores(int[] TestScores) {
+        this.TestScores = TestScores;
+    }
+
+
+    public double calculateAverage() {
+        if (TestScores.length == 0) {
+            return 0; // Return 0 if there are no test scores.
+        }
+
+        double total = 0.0;
+        for (int score : TestScores) {
+            if (score < 0 || score > 100) {
+                throw new IllegalArgumentException("Invalid test score: " + score);
+            }
+            total += score;
+        }
+
+        return total / TestScores.length;
+    }
+
+
+
+
+
+    public static void main (String[] args){
+            int[] testScores = {85, 90, 78, 95, 100, 118,}; // Example array of test scores
+            TestScores test = new TestScores(testScores);
+
+            try {//Note when one exception is found it stops the program after it throws
+                double average = test.calculateAverage();
+                System.out.println("Average test score: " + average);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Error: " + e.getMessage());
+            } finally {
+                System.out.println("Done");
+
             }
 
         }
     }
-}
+
