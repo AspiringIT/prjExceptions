30ab94d2b2754baea5ed1187cc8b6395a5dd7a1b
HappyRunner389 <nicktab20@gmail.com>
2023-09-28 15:40:41 -0500

    Got it to still calculate avg but can only handle one invaild number

--- src/TestScores.java
+++ src/TestScores.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
 public class TestScores {
+    static double Total = 0;
+    static int ValidArg;
     private double[] TestScores; //initalizes Test scores array
 
     //class constructor
@@ -14,15 +17,20 @@ public class TestScores {
 
         double total = 0.0; //initalizes total variable
         for (double score : TestScores) {
+            Total=total;
+            ValidArg++;
+            System.out.println(score);
+            total += score; //this has score added to it at each item in the array
             if (score < 0 || score > 100) {//checks if the value is less than zero OR if the value is greater than 100
                 //if either is true it throws the below exception
+                Total -= score;
+                ValidArg = ValidArg - 1;
                 throw new IllegalArgumentException("Invalid test score: " + score +
-                        " The average could not be calculated due to an invalid test score.");
-            }
-            total += score; //this has score added to it at each item in the array
+                        " The item will be skipped.");
+                }
         }
 
-        return total / TestScores.length; //This calculates the avg by the total value divided by the number of items
+        return total/ValidArg; //This calculates the avg by the total value divided by the number of items
     }
 
 
@@ -30,7 +38,7 @@ public class TestScores {
 
 
     public static void main (String[] args){
-            double[] testScores = {78,97.2,67,83,100,113}; //  array of test scores
+            double[] testScores = {-1,78,97.2,113,89}; //  array of test scores
             TestScores test = new TestScores(testScores); // creates a new object of TestScores and names the object test
 
             try {//Note when one exception is found it stops the program after it throws
@@ -39,7 +47,7 @@ public class TestScores {
             } catch (IllegalArgumentException e) {
                 System.err.println("Error: " + e.getMessage());
             } finally {//This will ALWAYS execute even if there is no exception
-
+                System.out.println("This is avg " + Total / ValidArg);
                 System.out.println("Done");
 
             }
